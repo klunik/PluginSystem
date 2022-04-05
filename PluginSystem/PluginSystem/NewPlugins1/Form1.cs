60b612c82ccd990263ff6bbbe710bcfd77cc84aa
klunik <62893628+klunik@users.noreply.github.com>
2022-04-05 22:45:04 +0300

    Add files via upload

---
+++ PluginSystem/PluginSystem/NewPlugins1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewMainInterface;
+namespace NewPlugins1
+{
+    public partial class Form1 : Form
+    {
+        private readonly IPlugin _pl;
+        public Form1(IPlugin pl)
+        {
+            InitializeComponent();
+            this._pl = pl;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PluginSystem/PluginSystem/PluginSystem/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewMainInterface;
+namespace PluginSystem
+{
+    public partial class MainWindow : Form, IPluginHost
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public bool Register(IPlugin plug)
+        {
+            return true;
+        }
+    }
+}
